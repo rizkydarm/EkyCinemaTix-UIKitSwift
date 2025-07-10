d7e3ea30b050d653467e6d3595fadc0aea064c20
rizkyeky <mochamad.rizky.darmawan>
2025-07-11 06:56:18 +0700

    Initial Commit

---
+++ EkyCinemaTix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EkyCinemaTix
+//
+//  Created by Eky on 11/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
